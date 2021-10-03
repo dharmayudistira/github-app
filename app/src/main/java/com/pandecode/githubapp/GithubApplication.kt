101b6775ca85ed2f6de59a16a460ac84f97df61c
Dharma Yudistira Eka Putra <dharmayudistira2000@gmail.com>
2021-10-03 12:39:44 +0700

    fix: migrating from glide to coil

--- app/src/main/java/com/pandecode/githubapp/GithubApplication.kt
+++ app/src/main/java/com/pandecode/githubapp/GithubApplication.kt
@@ -1,14 +1,18 @@
 package com.pandecode.githubapp
 
 import android.app.Application
+import coil.ImageLoader
+import coil.ImageLoaderFactory
+import coil.util.CoilUtils
 import com.pandecode.data.di.networkModule
 import com.pandecode.data.di.repositoryModule
 import com.pandecode.githubapp.di.useCaseModule
 import com.pandecode.githubapp.di.viewModelModule
+import okhttp3.OkHttpClient
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
-class GithubApplication : Application() {
+class GithubApplication : Application(), ImageLoaderFactory {
     override fun onCreate() {
         super.onCreate()
 
@@ -24,5 +28,16 @@ class GithubApplication : Application() {
             )
         }
 
+    }
+
+    override fun newImageLoader(): ImageLoader {
+        return ImageLoader.Builder(this@GithubApplication)
+            .crossfade(true)
+            .okHttpClient {
+                OkHttpClient.Builder()
+                    .cache(CoilUtils.createDefaultCache(this@GithubApplication))
+                    .build()
+            }
+            .build()
     }
 }
--- app/src/main/java/com/pandecode/githubapp/utils/ViewExtension.kt
+++ app/src/main/java/com/pandecode/githubapp/utils/ViewExtension.kt
@@ -1,11 +1,17 @@
 package com.pandecode.githubapp.utils
 
 import android.widget.ImageView
-import com.bumptech.glide.Glide
+import coil.imageLoader
+import coil.load
+import coil.transform.CircleCropTransformation
 
 fun ImageView.loadAsCircle(source: String) {
-    Glide.with(this.context)
-        .load(source)
-        .circleCrop()
-        .into(this)
+    val imageLoader = context.imageLoader
+
+    this.load(
+        uri = source,
+        imageLoader = imageLoader
+    ) {
+        transformations(CircleCropTransformation())
+    }
 }
